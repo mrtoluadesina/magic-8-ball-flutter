8dac07ff32b2142dfd81851ddcf262d9decb5f04
Tolu Adesina <tadesina90@gmail.com>
2019-07-31 13:27:19 +0100

    Layout and ui done

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MagicApp(),
+    );
+
+class MagicApp extends StatefulWidget {
+  @override
+  _MagicAppState createState() => _MagicAppState();
+}
+
+class _MagicAppState extends State<MagicApp> {
+  int anyBall = 1;
+  void answer() {
+    setState(() {
+      anyBall = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Magic 8 Ball'),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: Center(
+          child: FlatButton(
+            onPressed: () {
+              answer();
+            },
+            child: Image.asset('ball$anyBall.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
